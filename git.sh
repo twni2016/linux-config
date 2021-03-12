d4db9b68ae61d3a84aeb0106f74cffa27f35b149
Tianwei V. Ni <twni2016@gmail.com>
2021-03-11 20:30:24 -0500

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,3 +1,5 @@
+# all these commands save config in ~/.gitconfig
 git config --global user.name "twni2016"
 git config --global user.email "twni2016@gmail.com"
 git config --global credential.helper store
+git config --global color.ui auto # make git ui (git status) display color

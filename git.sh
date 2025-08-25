abe1a7458473b66c26c7300f0eff5216958eef42
Tianwei Ni <twni2016@gmail.com>
2025-08-25 16:35:28 -0400

    Update git.sh

--- git.sh
+++ git.sh
@@ -2,3 +2,4 @@
 git config --global user.name "twni2016"
 git config --global user.email "twni2016@gmail.com"
 git config --global color.ui auto # make git ui (git status) display color
+git config --global credential.helper store
